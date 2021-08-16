ef7b9f8128c540a66eec0e5f62814737709aae30
nihalshameem <nihalshameem6@gmail.com>
2021-08-16 23:39:06 +0530

    scartch page design

--- src/Modules/ScratchCards.js
+++ src/Modules/ScratchCards.js
@@ -6,21 +6,10 @@ export default function ScratchCards(props) {
   const [loader, setLoader] = useState(true);
   const [scratched, setScratched] = useState([]);
   const [unscratched, setUnscratched] = useState([]);
+  const [tabs, setTabs] = useState("1");
 
   useEffect(() => {
     checkStorage();
-  }, []);
-
-  useEffect(() => {
-    scatchCardsApi()
-      .then((res) => {
-        setScratched(res.data.scratched);
-        setUnscratched(res.data.unscratched);
-      })
-      .catch((e) => {
-        console.log(e.response);
-      });
-    setLoader(false);
   }, []);
 
   function checkStorage() {
@@ -39,31 +28,114 @@ export default function ScratchCards(props) {
       props.history.push("/login");
     }
   }
+
+  useEffect(() => {
+    scatchCardsApi()
+      .then((res) => {
+        setScratched(res.data.scratched);
+        setUnscratched(res.data.unscratched);
+        setLoader(false);
+      })
+      .catch((e) => {
+        console.log(e.response);
+        setLoader(false);
+      });
+  }, []);
+  function tabChange(tab) {
+    setTabs(tab);
+  }
   return (
-    <div className="container">
+    <div className="container" style={{ paddingTop: "100px" }}>
       {loader && <LoaderMini />}
-      <div className="row">
-        {scratched &&
-          scratched.map((item) => (
+      <div class="tabbable-panel">
+        <div class="tabbable-line">
+          <ul class="nav nav-tabs ">
+            <li class={` ${tabs === "1" ? "active" : ""}`}>
+              <a
+                href="#tab_default_1"
+                data-toggle="tab"
+                onClick={() => {
+                  tabChange("1");
+                }}
+              >
+                Unscratched
+              </a>
+            </li>
+            <li class={` ${tabs === "2" ? "active" : ""}`}>
+              <a
+                href="#tab_default_2"
+                data-toggle="tab"
+                onClick={() => {
+                  tabChange("2");
+                }}
+              >
+                Scratched
+              </a>
+            </li>
+          </ul>
+          <div class="tab-content">
             <div
-              className="col-xs-12 col-sm-12 col-md-6 col-lg-4"
-              key={item.id}
+              class={`tab-pane ${tabs === "1" ? "active" : ""}`}
+              id="tab_default_1"
             >
-              <div className="cash-box">
-                <div className="img">
-                  <img
-                    src={imageUrl(item.image)}
-                    className="img-responsive"
-                    alt={item.name}
-                  />
-                </div>
-                <div>
-                  <p>{item.amount}</p>
-                  <button className="btn btn-lg btn-primary">Bid</button>
-                </div>
+              <div className="row">
+                {scratched &&
+                  scratched.map((item) => (
+                    <div
+                      className="col-xs-12 col-sm-12 col-md-6 col-lg-4"
+                      key={item.id}
+                    >
+                      <div className="cash-box">
+                        <div className="img">
+                          <img
+                            src={imageUrl(item.image)}
+                            className="img-responsive"
+                            alt={item.name}
+                          />
+                        </div>
+                        <div>
+                          <p>{item.amount}</p>
+                          <button className="btn btn-lg btn-primary">
+                            Bid
+                          </button>
+                        </div>
+                      </div>
+                    </div>
+                  ))}
               </div>
             </div>
-          ))}
+            <div
+              class={`tab-pane ${tabs === "2" ? "active" : ""}`}
+              id="tab_default_2"
+            >
+              <div className="row">
+                {unscratched &&
+                  unscratched.map((item) => (
+                    <div
+                      className="col-xs-12 col-sm-12 col-md-6 col-lg-4"
+                      key={item.id}
+                    >
+                      <div className="cash-box">
+                        <div className="img">
+                          <img
+                            src={imageUrl(item.image)}
+                            className="img-responsive"
+                            alt={item.name}
+                          />
+                        </div>
+                        <div>
+                          <p>{item.amount}</p>
+                          <button className="btn btn-lg btn-primary">
+                            Bid
+                          </button>
+                        </div>
+                      </div>
+                    </div>
+                  ))}
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
